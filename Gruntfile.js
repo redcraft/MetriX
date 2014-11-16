04796ccaa53d4305ce79c1dab48bd180da051414
Maxim Gurkin <redmax3d@gmail.com>
2014-11-16 21:35:08 +0500

    Adding html templates minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,28 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		ngtemplates:  {
+			metrix:        {
+				src:      'src/main/webapp/components/*.html',
+				dest:     'src/main/webapp/min/templates.js'
+			}
+		},
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
 			build: {
-				src: ['src/main/webapp/libs/*.js', 'src/main/webapp/app.js', 'src/main/webapp/components/*.js'],
+				src: ['src/main/webapp/libs/*.js', 'src/main/webapp/app.js', 'src/main/webapp/min/templates.js', 'src/main/webapp/components/*.js'],
 				dest: 'src/main/webapp/min/app.min.js'
 			}
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-angular-templates');
 
-	grunt.registerTask('default', ['uglify']);
-	grunt.registerTask('heroku', ['uglify']);
+	grunt.registerTask('default', ['ngtemplates', 'uglify']);
+	grunt.registerTask('heroku', ['ngtemplates', 'uglify']);
 
 };
