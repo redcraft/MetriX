f13e59e4fb1db9b27caad3a475ff0d65b2cbd5db
Maxim Gurkin <redmax3d@gmail.com>
2014-11-16 20:12:42 +0500

    Added javascript minification

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: ['src/main/webapp/libs/*.js', 'src/main/webapp/app.js', 'src/main/webapp/components/*.js'],
+				dest: 'src/main/webapp/app.min.js'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', ['uglify']);
+
+};
