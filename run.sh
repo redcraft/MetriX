94ded492cd3e618fac13aadb44f20d9d365a347b
Maxim Gurkin <redmax3d@gmail.com>
2017-03-07 03:08:54 +0800

    Run script added to simplify start up at local machine

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export API_KEY=
+export API_SECRET=
+export FIXED_THREAD_POOL_SIZE=3
+
+if [[ -z $API_KEY || -z $API_SECRET || -z $FIXED_THREAD_POOL_SIZE ]]
+then 
+	echo "[ERROR] Dropbox API_KEY and API_SECRET are not set."; 
+else 
+	mvn clean && npm install && grunt && mvn jetty:run
+fi
