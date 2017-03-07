d4820d6f94a58846c43a661d6d3ed5c7256f0ff0
Maxim Gurkin <redmax3d@gmail.com>
2017-03-08 06:08:01 +0800

    Added possibility to explicitly set production build mode (js/css obfuscation enabled)

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 export API_KEY=
 export API_SECRET=
 export FIXED_THREAD_POOL_SIZE=3
+export PRODUCTION_BUILD=false
 
 if [[ -z $API_KEY || -z $API_SECRET || -z $FIXED_THREAD_POOL_SIZE ]]
 then 
 	echo "[ERROR] Dropbox API_KEY and API_SECRET are not set."; 
 else 
-	mvn clean && npm install && grunt && mvn jetty:run
+	mvn clean && npm install && grunt && mvn jetty:run-war
 fi
